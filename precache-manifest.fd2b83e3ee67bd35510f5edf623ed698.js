332bbf4ac9e39de4d4f7c2df0a4952454e684baf
lawrence ardosa <lawrenceardosa@gmail.com>
2020-10-09 19:52:15 +0800

    Updates

---
+++ precache-manifest.fd2b83e3ee67bd35510f5edf623ed698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc40607484aa941dcda7323c7b756abd",
+    "url": "/wine-ebi/index.html"
+  },
+  {
+    "revision": "9bf2c09ac957dfdd2fdb",
+    "url": "/wine-ebi/static/css/2.f1260932.chunk.css"
+  },
+  {
+    "revision": "fb8dd03c4fe7d518e505",
+    "url": "/wine-ebi/static/css/main.c371c6b2.chunk.css"
+  },
+  {
+    "revision": "9bf2c09ac957dfdd2fdb",
+    "url": "/wine-ebi/static/js/2.c56f6479.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wine-ebi/static/js/2.c56f6479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8dd03c4fe7d518e505",
+    "url": "/wine-ebi/static/js/main.a8922a5e.chunk.js"
+  },
+  {
+    "revision": "63a3d7d709373150c566",
+    "url": "/wine-ebi/static/js/runtime-main.3e4a7f2e.js"
+  }
+]);
